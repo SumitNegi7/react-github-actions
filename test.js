335c0f25b6cc0082e6726d0998276bf00edd9aca
SumitNegi7 <sumitnegi2142@gmail.com>
2021-03-28 21:09:28 +0530

    Run e2e test on CI

---
+++ test.js
@@ -0,0 +1,9 @@
+const { firefox } = require('playwright');
+
+(async () => {
+  const browser = await firefox.launch({ headless: false, slowMo: 50 });
+  const page = await browser.newPage();
+  await page.goto('http://whatsmyuseragent.org/');
+  await page.screenshot({ path: `example.png` });
+  await browser.close();
+})();
