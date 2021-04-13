41b15d4277fe3a0ecee1de9493c98b3ba3e7e051
SumitNegi7 <sumitnegi2142@gmail.com>
2021-04-13 12:10:48 +0530

    update steps in workflow

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-const { firefox } = require('playwright');
+const { chromium } = require('playwright');
 
 (async () => {
-  const browser = await firefox.launch({ headless: false, slowMo: 50 });
+  const browser = await chromium.launch({ headless: false, slowMo: 50 });
   const page = await browser.newPage();
   await page.goto('http://whatsmyuseragent.org/');
   await page.screenshot({ path: `example.png` });
